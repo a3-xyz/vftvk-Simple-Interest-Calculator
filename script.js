9928c28ae76f28571241be7d2cf61223ce9b80b6
AV <archanavijay2001@gmail.com>
2021-06-25 06:49:20 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+     var principal = document.getElementById("principal").value;
+
+     //To check if principal is 0 or negative
+    if(principal < 1)
+    {
+        alert("Please enter a positive value");
+        document.getElementById("principal").focus();
+        return false;
+    }
+     var rate = document.getElementById("rate").value;
+     var years = document.getElementById("years").value;
+     //To Calculate Interest
+     var interest = principal * years * rate /100;
+     var year = new Date().getFullYear()+parseInt(years) ;
+     var amount = principal + interest
+     //To show the result
+     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval +"%";
 }
         
